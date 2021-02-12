b884fc8ff483463b96a3210b000347e412dd30f4
Valentin Magnan <>
2021-02-12 16:17:57 +0100

    RE-Initialisation

---
+++ app/src/main/java/com/isen/m2/handfullproject/handfullprojectandroidapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.isen.m2.handfullproject.handfullprojectandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
